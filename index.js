d6efeba94194f5e1aedff19da39931145f30e1cb
Mulbrix <54961232+Mulbrix@users.noreply.github.com>
2021-04-17 19:56:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const fetch = require("node-fetch");
 const client = new Discord.Client();
 const token = process.env['token']
 
+const sadWords = ["sad", "depressed", "unhappy", "angry"];
+const encouragements = ["Cheer up!", 
+"Hang in there.", 
+"You are a great person / bot!"
+];
+
 function getQuote() {
   return fetch("https://zenquotes.io/api/random").then(result => {
     return result.json()
@@ -22,6 +28,11 @@ client.on("message", message => {
 
   if (message.content === "$inspire") {
     getQuote().then(quote => message.channel.send(quote))
+  }
+
+  if (sadWords.some(word => message.content.includes(word))) {
+    const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)]
+    message.reply(encouragement)
   }
 });
 
